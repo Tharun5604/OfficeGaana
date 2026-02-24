f3b3535e05207f3f19d18db9bccd5824edff51a0
Tharun K <tharun.jansi@gmail.com>
2026-02-24 06:13:09 +0530

    gallery changes

--- src/components/AnimatedGallery.jsx
+++ src/components/AnimatedGallery.jsx
@@ -8,6 +8,8 @@ const images = [
   "/images/gallery/1.jpg",
   "/images/gallery/2.jpg",
   "/images/gallery/3.jpg",
+  "/images/gallery/4.jpg",
+  "/images/gallery/5.jpg",
   "/images/gallery/6.jpg",
   "/images/gallery/7.jpg",
   "/images/gallery/9.jpg",
@@ -15,8 +17,6 @@ const images = [
   "/images/gallery/11.jpg",
   "/images/gallery/12.jpg",
   "/images/gallery/8.jpg",
-  "/images/gallery/4.jpg",
-  "/images/gallery/5.jpg",
 ];
 
 const soundFiles = [
